e4e8d621ae9b899baaf93824dbf1f23e636b6187
mishrabhii <abhi.mishra509@gmail.com>
2022-09-15 18:08:01 +0530

    calculate length of 2 Points (x1,y1)(x2,y2)

--- Line_Comparison/src/com/lineComparison/LineComparison.java
+++ Line_Comparison/src/com/lineComparison/LineComparison.java
@@ -4,5 +4,12 @@ public class LineComparison {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Computation Program ");
+
+		int x1,x2,y1,y2;
+		double distance;
+		x1=1;y1=1;x2=4;y2=4;
+		distance=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));	 	    
+		System.out.println("distance between = "+"("+x1+","+y1+"),"+"("+x2+","+y2+") "+distance);
 	}
+
 }
