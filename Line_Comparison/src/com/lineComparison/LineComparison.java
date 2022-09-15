e6f690d2a64c7b6ff0b76c31d8fe28f976577e6f
mishrabhii <abhi.mishra509@gmail.com>
2022-09-15 18:03:06 +0530

    initial commit

---
+++ Line_Comparison/src/com/lineComparison/LineComparison.java
@@ -0,0 +1,8 @@
+package com.lineComparison;
+
+public class LineComparison {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Computation Program ");
+	}
+}
