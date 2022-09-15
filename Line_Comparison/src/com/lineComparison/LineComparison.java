1fb1bd5e3d7c190b764caac4bd14adc54ee36436
mishrabhii <abhi.mishra509@gmail.com>
2022-09-15 20:46:14 +0530

    compare two lines based on the end points

--- Line_Comparison/src/com/lineComparison/LineComparison.java
+++ Line_Comparison/src/com/lineComparison/LineComparison.java
@@ -12,13 +12,26 @@ public class LineComparison {
 		System.out.println("distance between = "+"("+x1+","+y1+"),"+"("+x2+","+y2+") "+distance);
 
 		int lengthLine1 = 2;
-		int lengthLine2 = 2;		
+		int lengthLine2 = 5;		
 		int d1 = (int) (lengthLine1);
 		int d2 = (int)(lengthLine2);
 		if (d1==(d2))
 			System.out.println("Line 1 and line 2 have Equal lengths.");
 		else
 			System.out.println("Line 1 and Line 2 do not have Equal lengths.");
-	}
+		
+		if ((Double.compare(lengthLine1, lengthLine2)) == 0) 
+			System.out.println("Length of Line 1 is equal to the length of Line 2.");
+			else 
+				if ((Double.compare(lengthLine1, lengthLine2)) < 0) 
+					System.out.println("Length of Line 1 is less than the length of Line 2.");
+				else
+					if ((Double.compare(lengthLine1, lengthLine2)) > 0) 
+						System.out.println("Length of Line 1 is greater than the length of Line 2.");
+
+
+		
+	}			
+
 }
 
